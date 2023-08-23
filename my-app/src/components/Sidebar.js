af3e6af1e6e20202289b4bbc29447b43d7878d0f
Myekman <my@traningstemplet.se>
2023-08-23 12:08:32 +0000

    add css style to react app

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import css from './css/Sidebar.module.css';
 
 function Sidebar() {
     return (
         <div>
-            <a href = "#." target= "_blank">My Photos</a>
-            <a href = "#." target= "_blank">My Illustrations</a>
-            <a href = "#." target= "_blank">My Paintings</a>
+            <a href = "#." className ={css.sidebar}  target= "_blank">My Photos</a>
+            <a href = "#." className ={css.sidebar}  target= "_blank">My Illustrations</a>
+            <a href = "#." className ={css.sidebar} target= "_blank">My Paintings</a>
         </div>
     )
 }
